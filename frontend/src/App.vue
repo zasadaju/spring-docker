888bfb83bbf1bcd1050865b8a465141f47403f10
--global <--global>
2025-01-31 20:07:06 +0100

    fix

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,13 @@
 <template>
   <div>
-    <nav>
       <NavbarComponent />
       <router-view></router-view>
-    </nav>
   </div>
 </template>
 
 <script>
   import NavbarComponent from './components/NavbarComponent.vue';
   export default {
-    components: {NavbarComponent}
+      components: { NavbarComponent }
   };
 </script>
