9548ad0c2bf1948036373e74ab10d9678b44ae45
--global <--global>
2025-01-31 20:13:56 +0100

    fix

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,15 +1,17 @@
 // const { defineConfig } = require('@vue/cli-service')
 // module.exports = defineConfig({
+
 //   transpileDependencies: true
 // })
+
 
 module.exports = {
   devServer: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        pathRewrite: {'/api': ''},
+      '/api': { //każde żądanie zaczynającie się od /api będzie przekierowane
+        target: 'http://localhost:8080', //docelowy backend działa porcie 8080
+        changeOrigin: true, //podmiana nagłówka origin w żądaniu
+        pathRewrite: {'^/api':''}, //usuwamy /api z url przed przekazaniem do backendu
       },
     }
   }
